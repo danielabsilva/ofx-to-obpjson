608ad21be0282549d3a3b4da9740d452df082a15
Ismail CHAIB <i_chaib@esi.dz>
2012-11-09 20:29:02 +0000

    still can't post to the api

--- rockandroll.py
+++ rockandroll.py
@@ -36,7 +36,7 @@ def to_obp_json(account_id, account_holder, ofx_file):
       details = {
            "type_en":transaction.type,
            "type_de":transaction.type,
-           "posted":{"$dt":str(transaction.date) },
+           "posted":{"$dt":str(transaction.date.astimezone(GMT())) },
            "completed":{"$dt":str(transaction.date)},
            "new_balance":{
                 "currency":ofx.account.statement.currency,
@@ -51,7 +51,7 @@ def to_obp_json(account_id, account_holder, ofx_file):
       
       obp_transaction = {'this_account':this_account, 'other_account':other_account, 'details' : details }
 
-      transactions.append(obp_transaction)
+      transactions.append({'obp_transaction':obp_transaction})
 
     #Serializing the object
     obpjson = json.dumps(transactions)
@@ -65,13 +65,13 @@ def post_to_api():
     data = to_obp_json("4300-1-50180-8", 'Hacker Transparencia', 'sample.ofx')
     f1=open('./json.txt', 'w+')
     print >> f1, data
-    url = 'https://demo.openbankproject.com/api/tmp/transactions?secret=Ijqds901wla#920xmlz?'    
+    url = 'https://demo.openbankproject.com/api/tmp/transactions?secret=Ijqds901wla920xmlz'    
     #headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
     req = urllib2.Request(url, data, {'Content-type': 'application/json'})
     print url
     f = urllib2.urlopen(req)
     response = f.read()
-    print "response: " + str(response)
+    print "response: " + response
     f.close()
     
 
