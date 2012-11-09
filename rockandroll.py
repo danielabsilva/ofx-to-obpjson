f44ce73ef7faa86a7b55abe9c235e363a1cc7062
Ismail CHAIB <i_chaib@esi.dz>
2012-11-09 16:09:43 +0000

    Algorithm to get the right JSON written

--- rockandroll.py
+++ rockandroll.py
@@ -2,24 +2,34 @@ from ofxparse import OfxParser
 import json
 
 #TODO: get rid of sample.ofx
-def to_obpjson(ofxfile):
+def to_obp_json(ofx_file):
     #Initiate the ofx object
-    ofx = OfxParser.parse(file(ofxfile))
-    obptransaction = {}
-    
+    ofx = OfxParser.parse(file(ofx_file))
+    transactions = []
     #TODO making sure end-date and start-date doesnt clash 
     for transaction in ofx.account.statement.transactions:
-      #obptransaction['id'] = ofx.account.number
-      print transaction.type
+      obp_transaction = {}
+      #THIS HOLDER
+
+      this_holder = {'name':ofx.account.number}
+      #details = {'amount': ofx.account.statement.transactions.amount }
+      
+      other_holder = {}
+      obp_transaction = {'this_holder':this_holder, 'other_holder':other_holder, 'other_date':'' }
+
+      transactions.append(obp_transaction)
+
     #Serializing the object
-    print 'JSON before serializing= ' + str(obptransaction)
-    obpjson = json.dumps(obptransaction)
+    print 'JSON before serializing= ' + str(obp_transaction)
+    obpjson = json.dumps(transactions)
     
     #Return the newly created json
     print '------------------------'
     print 'JSON = ' + obpjson
     return obpjson
 
+
+
 if __name__ == "__main__":
-    to_obpjson('sample.ofx')
+    to_obp_json('sample.ofx')
 
